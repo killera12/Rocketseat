b386899395fa1a54cc9c168d46670da066b26fb1
Pedro Silva <pedropgd981@gmail.com>
2024-09-30 16:36:55 -0300

    prototype chain

---
+++ js/classroom/script-008.js
@@ -0,0 +1,12 @@
+const address = {
+  city: "São Paulo",
+  country: "Brazil",
+}
+
+console.log(address)
+
+const users = ["José", "Maria", "João"]
+console.log(users)
+
+let userName = "José Junior"
+console.log(userName.__proto__)
