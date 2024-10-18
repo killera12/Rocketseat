b229ceb76eed4739971d8ced34b39a7c2997a4e3
Pedro Silva <pedropgd981@gmail.com>
2024-10-18 15:53:01 -0300

    format currency

--- js/projeto-11/refund/scripts.js
+++ js/projeto-11/refund/scripts.js
@@ -1,8 +1,24 @@
 // Seleciona os elementos do formulário
 const amount = document.querySelector("#amount")
 
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
+  // Obtém o valor do input e remove os caracteres não numéricos.
   let value = amount.value.replace(/\D/g, "")
 
-  amount.value = value
+  // Transformar o valor em centavos (exemplo: 150/100 = 1.5 que é igual a R$ 1,50).
+  value = Number(value) / 100
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL (Real Brasileiro)
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  // Retorna o valor formatado.
+  return value
 }
